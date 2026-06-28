edf6bfbbc003453883672acada5c1b0cefecf58f
leonbetgames <leonbetgames@gmail.com>
2026-06-28 18:11:51 +0100

    d

--- leon-games/src/App.jsx
+++ leon-games/src/App.jsx
@@ -1,28 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Discover from "./pages/Discover";
-import FAQ from "./pages/FAQ";
-import Support from "./pages/Support";
-import Privacy from "./pages/Privacy";
-import Terms from "./pages/Terms";
-import ResponsibleGaming from "./pages/ResponsibleGaming";
+function Dashboard() {
+  return <h1>DASHBOARD</h1>;
+}
 
-function App() {
+function Discover() {
+  return <h1>DISCOVER</h1>;
+}
+
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Discover />} />
-        <Route path="/faq" element={<FAQ />} />
-        <Route path="/support" element={<Support />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/terms" element={<Terms />} />
-        <Route
-          path="/responsible-gaming"
-          element={<ResponsibleGaming />}
-        />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/discover" element={<Discover />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
